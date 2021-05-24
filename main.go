44a541d0407172e3619ba42276106935d818abc9
liamchampton <liam.hampton@hotmail.co.uk>
2021-05-24 16:45:33 +0100

    iterate over the map

--- main.go
+++ main.go
@@ -5,23 +5,32 @@ import "fmt"
 func main() {
 
 	// option 1: map of keys + values of type string
-	// colors := map[string]string{
-	// 	"red":   "#ff000",
-	// 	"green": "#00FF00",
-	// }
+	colors := map[string]string{
+		"red":   "#ff000",
+		"green": "#00FF00",
+		"white": "#ffffff",
+	}
 
 	// option 2: map of keys + values of type string
 	// good if you want to figure out later on what to add to it
 	// var colors map[string]string
 
 	// option 3: map of keys + values of type string
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
-	colors["white"] = "#ffffff"
+	// colors["white"] = "#ffffff"
 
 	// delete element from map
-	delete(colors, "white")
+	// delete(colors, "white")
 
-	fmt.Println(colors)
-	fmt.Println(len(colors))
+	// fmt.Println(colors)
+	// fmt.Println(len(colors))
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
